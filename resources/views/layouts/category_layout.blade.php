fc1067ce705930b53aa70a08a3f1c264c4ffb50f
giangtran0501 <giangtran0501@gmail.com>
2016-08-08 20:05:52 -0700

    Update category_layout.blade.php
    Paging category

--- resources/views/layouts/category_layout.blade.php
+++ resources/views/layouts/category_layout.blade.php
@@ -16,5 +16,16 @@
     <div id='content' class='col-md-9 col-sm-9'>
         @yield('content_right')
     </div>
+    <div class="col-md-8 col-sm-8">
+    	<div class="pagination pull-right">
+	        <li><a href="#"> < </a></li>
+	        @for($i = 1; $i<= $pro_cate->lastPage(); $i = $i + 1)
+	            <li class="{!! ($pro_cate->currentPage() == $i) ? 'active' : '' !!}">
+	            	<a href="{!! $pro_cate->url($i) !!}">{{ $i }}</a>
+	            </li>
+	        @endfor
+	        <li><a href="#"> > </a></li>
+	    </div>
+    </div>
 </div>
 @stop
